a99e9920f8668ec146eb022b4a7a44d36801df77
Aseeva Julia <j.s.aseeva@mail.ru>
2021-01-16 20:18:40 +0300

    added fonts, base styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from 'react'
+
+import logoImg from './assets/images/logo-img.svg';
+import logoText from './assets/images/logo-text.svg';
+
 function App() {
   return (
-    <div className="App">
-      Main Page
+    <div className='wrapper'>
+      <div className='container app-section'>
+        <div className='mainPage_logoBlock'>
+          <div className='logo-block'>
+            <img src={logoImg} alt='loft taxi logo' className='logo-block__img' />
+            <img src={logoText} alt='loft taxi logo' className='logo-block__text' />
+          </div>
+        </div>
+        <div className='mainPage_formBlock'>
+          Здесь будет форма
+        </div>
+      </div>
     </div>
   );
 }
